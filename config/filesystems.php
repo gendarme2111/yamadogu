5f73ccc23a3d2353713d6e7a88bfa3faeb64a3aa
gendarme2111 <gendarme2111@gmail.com>
2020-10-23 05:17:34 +0900

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' =>env('AWS_SECRET_ACCESS_KEY'),
-            'region' =>env('AWS_DEFAULT_REGION'),
-            'bucket' =>env('AWS_BUCKET'),
+            'key' =>'AKIAY5HTCRQASUQO2TWI',
+            'secret' =>'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',
+            'region' =>'ap-northeast-1',
+            'bucket' =>'yamadoguapp',
             'url' =>'https://s3-ap-northeast-1.amazonaws.com/',
             'endpoint' =>env('AWS_ENDPOINT'),
         ],
